baa35a43057266ef6e5bc096576c529f576ac086
huohuade <houhaowen@gmail.com>
2025-07-15 11:47:03 +0800

    Update train.py

--- sft/train.py
+++ sft/train.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
         moba_chunk_size=args.moba_chunk_size,
         moba_topk=args.moba_topk,
     )
-    model.rwkv.emb.requires_grad_(False)
+    model.rwkv.emb.requires_grad_(False) # speed up 4x, but no effect on loss
     args.tokenizer = tokenizer
 
     train_data = MyDataset(args)
